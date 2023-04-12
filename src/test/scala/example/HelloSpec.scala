ce00b4cb308ccc0a8ce57bb0800af20f22fa5b19
ostrygun <oleg_strygun@intuit.com>
2023-04-12 12:59:38 -0700

    zio 2.0.12 and prep for zio tests/

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,10 +1,16 @@
-package example
+import zio.test._
+import zio.Console
+import zio.ZIO
+import zio.test.TestAspect._
+import zio.test.Assertion._
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class HelloSpec extends AnyFlatSpec with Matchers {
-  //"The Hello object" should "say hello" in {
-  //  Hello.greeting shouldEqual "hello"
- // }
+object HelloWorldSpec extends ZIOSpecDefault {
+  def spec =
+    suite("ZIO-QUARTZ-H2 tests")(
+      test("Say hello test") {
+        for {
+          _ <- Console.printLine("Hello ZIO Test")
+        } yield (assertTrue(true))
+      }
+    )
 }
