b46451db793e1a39cbbb1d26d122a282f1d61cd5
Oleg Strygun <ostrygun@gmail.com>
2025-03-15 20:29:11 -0700

    tests for all modes are ok -  ssl, nossl - nio, uring

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,4 +1,5 @@
 import zio.test._
+import zio.test.TestAspect
 import zio.Console
 import zio.{ZIO, ZLayer, Clock}
 import zio.durationInt
@@ -18,8 +19,14 @@ import io.quartz.http2.routes.WebFilter
 import io.quartz.http2.model.StatusCode
 
 object HelloWorldSpec extends ZIOSpecDefault {
+  //server shutdown with linux iouring is slow, it needs to wait until server times out. 
+  //hope to address this in a future with some sort of fiber canelation logic, 
+  //fiber sits on linux API call and canceling it is not easy.
 
   val linux = false
+  val noSSL_plain = false
+
+  val NUMBER_OF_STREAMS = 8
   val PORT = 11443
   val FOLDER_PATH = "/home/ols/web_root/"
   val BIG_FILE = "IMG_0278.jpeg"
@@ -29,6 +36,9 @@ object HelloWorldSpec extends ZIOSpecDefault {
   val env = ZLayer.fromZIO(ZIO.succeed("Bearer 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"))
 
   QuartzH2Server.setLoggingLevel(Level.INFO)
+
+  def buildSSLContext() =
+    if (noSSL_plain == false) QuartzH2Server.buildSSLContext("TLS", "keystore.jks", "password") else ZIO.succeed(null)
 
   val R: HttpRouteIO[Any] = {
     case req @ GET -> Root / "test" => ZIO.attempt(Response.Ok())
@@ -57,9 +67,8 @@ object HelloWorldSpec extends ZIOSpecDefault {
   def spec =
     suite("ZIO-QUARTZ-H2 tests")(
       test("Parallel retrieval with GET: client<-server") {
-        val NUMBER_OF_STREAMS = 30
         for {
-          ctx <- QuartzH2Server.buildSSLContext("TLS", "keystore.jks", "password")
+          ctx <- buildSSLContext()
           server <- ZIO.attempt(new QuartzH2Server("localhost", PORT.toInt, 15 * 1000, ctx))
 
           fib <-
@@ -67,7 +76,9 @@ object HelloWorldSpec extends ZIOSpecDefault {
             else (server.startIO(R, sync = false)).fork
 
           _ <- live(Clock.sleep(2000.milli))
-          c <- QuartzH2Client.open(s"https://localhost:$PORT", 10 * 1000, ctx)
+          c <-
+            if (noSSL_plain) QuartzH2Client.open(s"http://localhost:$PORT", 30 * 1000, ctx)
+            else QuartzH2Client.open(s"https://localhost:$PORT", 30 * 1000, ctx)
           program = c.doGet("/" + BIG_FILE).flatMap(_.stream.runCount)
           list <- ZIO.attempt(Array.fill(NUMBER_OF_STREAMS)(program))
           r <- ZIO.collectAllPar(list)
@@ -79,7 +90,7 @@ object HelloWorldSpec extends ZIOSpecDefault {
       },
       test("Proper 404 handling while sending data") {
         for {
-          ctx <- QuartzH2Server.buildSSLContext("TLS", "keystore.jks", "password")
+          ctx <- buildSSLContext()
           server <- ZIO.attempt(new QuartzH2Server("localhost", PORT.toInt, 16000, ctx))
 
           fib <-
@@ -87,7 +98,11 @@ object HelloWorldSpec extends ZIOSpecDefault {
             else (server.startIO(R, sync = false)).fork
 
           _ <- live(Clock.sleep(2000.milli))
-          c <- QuartzH2Client.open(s"https://localhost:$PORT", 1000, ctx)
+
+          c <-
+            if (noSSL_plain) QuartzH2Client.open(s"http://localhost:$PORT", 10 * 1000, ctx)
+            else QuartzH2Client.open(s"https://localhost:$PORT", 10 * 1000, ctx)
+
           file <- ZIO.attempt(new java.io.File(FOLDER_PATH + BIG_FILE))
           res <- c.doPost("/" + BIG_FILE, ZStream.fromFile(file))
           _ <- c.close()
@@ -97,10 +112,9 @@ object HelloWorldSpec extends ZIOSpecDefault {
         } yield (assertTrue(res.status.value == 404))
       },
       test("Parallel streams with POST") {
-        val NUMBER_OF_STREAMS = 30
         for {
-          ctx <- QuartzH2Server.buildSSLContext("TLS", "keystore.jks", "password")
-          server <- ZIO.attempt(new QuartzH2Server("localhost", PORT.toInt, 15 * 1000, ctx))
+          ctx <- buildSSLContext()
+          server <- ZIO.attempt(new QuartzH2Server("localhost", PORT.toInt, 50 * 1000, ctx))
 
           fib <-
             if (linux)(server.startIO_linuxOnly(1, R)).fork
@@ -110,7 +124,10 @@ object HelloWorldSpec extends ZIOSpecDefault {
           path <- ZIO.attempt(new java.io.File(FOLDER_PATH + BIG_FILE))
           present <- ZIO.attempt(path.exists())
           _ <- ZIO.fail(new java.io.FileNotFoundException(path.toString())).when(present == false)
-          c <- QuartzH2Client.open(s"https://localhost:$PORT", 10 * 1000, ctx)
+
+          c <-
+            if (noSSL_plain) QuartzH2Client.open(s"http://localhost:$PORT", 30 * 1000, ctx)
+            else QuartzH2Client.open(s"https://localhost:$PORT", 30 * 1000, ctx)
 
           program = for {
             fileStream <- ZIO.attempt(new java.io.FileInputStream(path))
@@ -148,7 +165,7 @@ object HelloWorldSpec extends ZIOSpecDefault {
 
         for {
           auth_string <- ZIO.environmentWith[String](str => str.get)
-          ctx <- QuartzH2Server.buildSSLContext("TLS", "keystore.jks", "password")
+          ctx <- buildSSLContext()
           server <- ZIO.attempt(new QuartzH2Server[String]("localhost", PORT.toInt, 15 * 1000, ctx))
 
           fib <-
@@ -156,7 +173,9 @@ object HelloWorldSpec extends ZIOSpecDefault {
             else (server.startIO(R, filter, sync = false)).fork
 
           _ <- live(Clock.sleep(2000.milli))
-          c <- QuartzH2Client.open(s"https://localhost:$PORT", 10 * 1000, ctx)
+          c <-
+            if (noSSL_plain) QuartzH2Client.open(s"http://localhost:$PORT", 10 * 1000, ctx)
+            else QuartzH2Client.open(s"https://localhost:$PORT", 10 * 1000, ctx)
           rsp <- c.doGet("/test", headers = Headers("Authorization" -> auth_string))
           rsp2 <- c.doGet("/test", headers = Headers("Authorization" -> "Bearer sl-12345"))
           _ <- server.shutdown
