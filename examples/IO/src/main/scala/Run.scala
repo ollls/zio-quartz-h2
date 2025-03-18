82939a785646fb7983a56e6a6cc3580b2d1b6106
Oleg Strygun <ostrygun@gmail.com>
2025-03-18 09:20:20 -0700

    upd message for demo apps

--- examples/IO/src/main/scala/Run.scala
+++ examples/IO/src/main/scala/Run.scala
@@ -155,6 +155,10 @@ object MyApp extends ZIOAppDefault {
   def run = {
     val env = ZLayer.fromZIO(ZIO.succeed("Hello ZIO World!"))
     (for {
+      _ <- zio.Console.printLine("****************************************************************************************")
+      _ <- zio.Console.printLine("\u001B[31mUse https://localhost:8443/doc/index.html to read the index.html file\u001B[0m")
+      _ <- zio.Console.printLine("****************************************************************************************")
+
       args <- this.getArgs
       _ <- ZIO.when(args.find(_ == "--debug").isDefined)(ZIO.attempt(QuartzH2Server.setLoggingLevel(Level.DEBUG)))
       _ <- ZIO.when(args.find(_ == "--error").isDefined)(ZIO.attempt(QuartzH2Server.setLoggingLevel(Level.ERROR)))
