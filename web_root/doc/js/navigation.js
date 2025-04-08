c39e9cf7c604b06378f00533b84d3dfe73d32fd1
Oleg Strygun <ostrygun@gmail.com>
2025-04-08 10:05:49 -0700

    navigation

--- web_root/doc/js/navigation.js
+++ web_root/doc/js/navigation.js
@@ -19,6 +19,15 @@ document.addEventListener('DOMContentLoaded', function() {
           </a>
         </li>
         <li>
+          <a href="/doc/settings.html" class="flex items-center px-4 py-3 text-gray-700 ${currentPath.includes('/settings.html') ? 'active-menu-item' : ''} menu-item">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <span class="font-medium">Settings</span>
+          </a>
+        </li>
+        <li>
           <a href="/doc/routes.html" class="flex items-center px-4 py-3 text-gray-700 ${currentPath.includes('/routes.html') ? 'active-menu-item' : ''} menu-item">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7" />
